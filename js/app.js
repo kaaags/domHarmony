42621076ee9186f764bdc3fe7934bbae5fc6c7cb
Sean Kagawa <seankagawa@gmail.com>
2018-02-21 21:53:01 -1000

    compl ex4

--- js/app.js
+++ js/app.js
@@ -10,11 +10,16 @@ var chgAge = document.getElementById("age").innerHTML = "21 year old vibrant stu
 
 // 3. Target the span element with the id of "distance" and change the number from 1000 to 168.
 
-
+var chgDistance = document.getElementById("distance").innerHTML = 168;
 
 // 4. Create a paragraph element with the id of "pro2". In this paragraph, add the following content: "My main objective is to find a Mrs. McDonald." Append this element inside of the paragraph element with the id of "profile".
 
+var para = document.createElement("p");
+var node = document.createTextNode("My main objective is to find a Mrs. McDonald.");
+para.appendChild(node);
+
+var element = document.getElementById("profile");
+element.appendChild(para);
 
 // 5. Create a span element of the id of "mail" and give it a content of 11. Append this element into the "Messages" li element.
-
 
